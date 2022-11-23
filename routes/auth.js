281ca47146e13e317994c6fb741ff42ef732acdd
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-23 15:08:24 +0100

    login with token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+const isAuthenticated = require("../middlewares/isAuthenticated");
 
 router.get("/signup", (req, res, next) => {
   res.json("Welcome to signup");
@@ -36,7 +38,21 @@ router.post("/login", async (req, res) => {
       res.json({ errorMessage: "No user with this username" });
     } else {
       if (bcrypt.compareSync(password, currentUser.password)) {
-        res.status(201).json({ msg: "Succesfully logged in" });
+        const userCopy = { ...currentUser._doc };
+        delete userCopy.password;
+        const authToken = jwt.sign(
+          {
+            expiresIn: "6h",
+            user: userCopy,
+          },
+          process.env.TOKEN_SECRET,
+          {
+            algorithm: "HS256",
+          }
+        );
+        res
+          .status(200)
+          .json({ msg: "Succesfully logged in", token: authToken });
       } else {
         res.status(400).json({ msg: "Incorrect password" });
       }
@@ -45,6 +61,16 @@ router.post("/login", async (req, res) => {
     console.log(error.message);
     res.json({ errorMessage: error.message });
   }
+});
+
+router.get("/verify", isAuthenticated, (req, res) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json({ payload: req.payload, message: "Token OK" });
 });
 
 module.exports = router;
