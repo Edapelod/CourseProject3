4d13ae194125c248f49c7e369ae498354cea1548
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-23 11:30:52 +0100

    user model created

--- models/User.model.js
+++ models/User.model.js
@@ -1,25 +1,30 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required.']
-    }
+const userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, "Username is required."],
+    unique: true,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-  }
-);
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  membership: {
+    type: String,
+    required: [true, "Please select one option"],
+    enum: ["Teacher", "Student"],
+  },
+});
 
 const User = model("User", userSchema);
 
---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+router.get("/signup", (req, res, next) => {
+  res.json("Welcome to signup");
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const salt = bcrypt.genSaltSync(11);
+    const passwordHash = bcrypt.hashSync(req.body.password, salt);
+    await User.create({
+      username: req.body.username,
+      password: passwordHash,
+    });
+    res.json("Succesfully created");
+  } catch (error) {
+    console.log(error.message);
+    res.json("auth/signup", { errorMessage: error.message });
+  }
+});
+
+router.get("/login", (req, res, next) => {
+  res.json("Login");
+});
+
+module.exports = router;
